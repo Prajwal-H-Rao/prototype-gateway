249314fa8ba3517c4d377f97a383b75b1f7e383c
Prajwal Rao <prajwalhr130@gmail.com>
2026-05-11 15:47:30 -0700

    setup server

---
+++ server.js
@@ -0,0 +1,16 @@
+import express, { response } from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/test/api", (req, response) => {
+  response.json({ message: "Hello from the server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
